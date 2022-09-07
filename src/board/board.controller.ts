a18ef0a1ea5646de730f82a375934db09176f140
Kim-Daehee123 <rkqkdxma@naver.com>
2022-09-07 17:36:31 +0900

    게시판 등록 API

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
 import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
   ApiOkResponse,
   ApiOperation,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
+import { multerOptions } from 'src/user/multerOptions';
 import { BoardService } from './board.service';
 import { SelectBoardMenuOutputDto } from './dto/board.menu.dto';
+import {
+  BoardRegisterInputDto,
+  BoardRegisterOutputDto,
+} from './dto/board.register.dto';
 
 @ApiTags('게시판 API')
 @Controller('board')
@@ -29,5 +49,46 @@ export class BoardController {
   })
   async getBoardMenu(): Promise<SelectBoardMenuOutputDto> {
     return await this.boardService.getBoardMenu();
+  }
+
+  //게시판 등록
+  @Post('/register')
+  @ApiOperation({ summary: '게시판 등록 API(완료)' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: '게시판 등록',
+    type: BoardRegisterInputDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: '게시판 등록 성공',
+    type: BoardRegisterOutputDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request',
+  })
+  @ApiResponse({
+    status: 401,
+    description: '인증 오류',
+  })
+  @ApiResponse({
+    status: 413,
+    description: '파일크기 제한',
+  })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard())
+  @UseInterceptors(FilesInterceptor('board', 10, multerOptions))
+  async boardRegister(
+    @Req() req,
+    @UploadedFiles() files: File[],
+    @Body(ValidationPipe)
+    boardRegisterInputDto: BoardRegisterInputDto,
+  ) {
+    return await this.boardService.boardRegister(
+      req.user,
+      boardRegisterInputDto,
+      files,
+    );
   }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,6 +1,9 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { createImageURL } from 'src/user/multerOptions';
 import { getConnection } from 'typeorm';
 import { SelectBoardMenuOutputDto } from './dto/board.menu.dto';
+import { BoardRegisterInputDto } from './dto/board.register.dto';
+import uuidRandom from './uuidRandom';
 
 @Injectable()
 export class BoardService {
@@ -36,6 +39,76 @@ export class BoardService {
         '게시판 등록 메뉴 조회 실패',
         HttpStatus.BAD_REQUEST,
       );
+    }
+  }
+
+  async boardRegister(
+    user_id: string,
+    boardRegisterInputDto: BoardRegisterInputDto,
+    files: File[],
+  ) {
+    const notice_id = uuidRandom();
+    const {
+      category,
+      title,
+      description,
+      link,
+      container_yn,
+      homemade_yn,
+      price,
+      how_share,
+      personnel,
+      deadline,
+    } = boardRegisterInputDto;
+    const conn = getConnection();
+    const date = new Date();
+
+    const sql = `INSERT INTO NOTICE_BOARD(NOTICE_ID, USER_ID, CATEGORY, TITLE, DESCRIPTION, LINK, 
+                 CONTAINER_YN, HOMEMADE_YN, PRICE, HOW_SHARE, PERSONNEL, DEADLINE, INSERT_DT, INSERT_ID) 
+                 VALUES(?,?,?,?,?,?,?,?,?,?,?,?,NOW(),?);`;
+    const params = [
+      notice_id,
+      user_id,
+      category,
+      title,
+      description,
+      link,
+      container_yn,
+      homemade_yn,
+      price,
+      how_share,
+      personnel,
+      deadline,
+      user_id,
+    ];
+
+    if (personnel < '2') {
+      this.logger.verbose(`User ${user_id} 참여 인원은 2명 이상 필요`);
+      throw new HttpException(
+        '참여 인원은 2명 이상 필요',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    try {
+      if (files.length) {
+        const arr = [];
+        for (const file of files) {
+          arr.push([notice_id, createImageURL(file), date, notice_id]);
+        }
+        const img_sql = `INSERT INTO NOTICE_BOARD_IMG(NOTICE_ID, NOTICE_IMG, INSERT_DT, INSERT_ID) VALUES ?;`;
+        await conn.query(img_sql, [arr]);
+      }
+      await conn.query(sql, params);
+
+      this.logger.verbose(`User ${user_id} 게시판 등록 성공`);
+      return {
+        statusCode: 201,
+        message: '게시판 등록 성공',
+      };
+    } catch (error) {
+      this.logger.verbose(`User ${user_id} 게시판 등록 성공 실패\n ${error}`);
+      throw new HttpException('게시판 등록 성공 실패', HttpStatus.BAD_REQUEST);
     }
   }
 }
